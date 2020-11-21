1f6c7ef3dedb5dc758570e840d9e9a5aa17cb1f2
jlzsss <jlz19821031@gmail.com>
2020-11-21 20:18:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 cat > /ssrbin/config.json <<EOF
 {
     "password": "${PASSWORD}",
-    "method": "aes-256-cfb",
-    "protocol": "auth_aes128_sha1",
+    "method": "chacha20-ietf",
+    "protocol": "origin",
     "protocol_param": "",
-    "obfs": "tls1.2_ticket_fastauth",
+    "obfs": "plain",
     "obfs_param": "",
 
     "udp": true,
